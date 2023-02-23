1f4bc2024ef64edf7ffd1b980afb6ca668ac6de1
Toms Savickis <tomssavickis7@gmail.com>
2023-02-23 18:33:47 +0200

    commit_1

--- main.py
+++ main.py
@@ -11,20 +11,29 @@ def are_matching(left, right):
 
 def find_mismatch(text):
     opening_brackets_stack = []
+    bracket_positions = {}
     for i, next in enumerate(text):
         if next in "([{":
-            # Process opening bracket, write your code here
-            pass
+            opening_brackets_stack.append(Bracket(next, i))
+
 
         if next in ")]}":
-            # Process closing bracket, write your code here
-            pass
+            if not opening_brackets_stack:
+                return i + 1
+            top = opening_brackets_stack.pop()
+            if not are_matching(top.char, next):
+                return i + 1
+            bracket_positions[top.position] = i
+            bracket_positions[i] = top.position
+    if opening_brackets_stack:
+        return opening_brackets_stack[0].position +1
+    return "Success"
 
 
 def main():
     text = input()
     mismatch = find_mismatch(text)
-    # Printing answer, write your code here
+    print(mismatch)
 
 
 if __name__ == "__main__":
